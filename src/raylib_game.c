a8db03f3a045fd61127b89c38ca13bcf92251cfe
César González Segura <contact@cesargonzalez.dev>
2026-07-09 16:22:00 +0200

    Day 2

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -31,60 +31,14 @@
     #define LOG(...)
 #endif
 
-
-float SmoothDamp(
-  float current,
-  float target,
-  float *current_velocity,
-  float smooth_time,
-  float max_speed,
-  float delta_time) {
- smooth_time = fmax(0.0001f, smooth_time);
- float num = 2.0f / smooth_time;
- float num2 = num * delta_time;
- float num3 = 1.0f / (1.0f + num2 + 0.48f * num2 * num2 + 0.235f * num2 * num2 * num2);
- float num4 = current - target;
- float num5 = target;
- float num6 = max_speed * smooth_time;
- num4 = Clamp(num4, -num6, num6);
- target = current - num4;
- float num7 = (*current_velocity + num * num4) * delta_time;
- *(current_velocity) = (*(current_velocity) - num * num7) * num3;
- float num8 = target + (num4 + num7) * num3;
- if ((num5 - current > 0.0f) == (num8 > num5))
- {
-  num8 = num5;
-  *(current_velocity) = (num8 - num5) / delta_time;
- }
- return num8;
-}
-
-Vector3 Vector3SmoothDamp(
-  Vector3 current,
-  Vector3 target,
-  Vector3 *current_velocity,
-  float smooth_time,
-  float max_speed,
-  float delta_time) {
-  float *vx = &(current_velocity->x);
-  float *vy = &(current_velocity->y);
-  float *vz = &(current_velocity->z);
-
-  float x = SmoothDamp(current.x, target.x, vx, smooth_time, max_speed, delta_time);
-  float y = SmoothDamp(current.y, target.y, vy, smooth_time, max_speed, delta_time);
-  float z = SmoothDamp(current.z, target.z, vz, smooth_time, max_speed, delta_time);
-
-  current_velocity->x = *(vx);
-  current_velocity->y = *(vy);
-  current_velocity->z = *(vz);
-
-  return (Vector3){ x, y, z };
-}
-
 static const int screenWidth = 720;
 static const int screenHeight = 720;
 static Model hexagon_model;
 static Model trinket_model;
+static Model hexbot_model;
+static ModelAnimation *hexbot_dance;
+static ModelAnimation *hexbot_spin;
+static ModelAnimation *hexbot_macarena;
 
 #define HEX_STATUS_GOOD 0
 #define HEX_STATUS_WARN 1
@@ -103,6 +57,7 @@ static Camera3D camera = {
     .fovy = 60.0f,
     .projection = CAMERA_PERSPECTIVE
 };
+static Vector3 end_camera_position = { 5.0f, 6.0f, 5.0f };
 
 static Vector3 player_position = (Vector3){ 0.0f, 0.0f, 0.0f };
 static float speed = 2.0f;
@@ -110,17 +65,66 @@ static Vector3 player_velocity = (Vector3){ 0.0f, 0.0f, 0.0f };
 static Vector3 player_target = (Vector3){ 0.0f, 0.0f, 0.0f };
 static int player_hexagon_x = NUM_HEXAGONS-1;
 static int player_hexagon_y = NUM_HEXAGONS-1;
-static float player_time = 1.0f;
+static float player_y_lerp_t = 1.0f;
+static float player_xz_lerp_t = 0.0f;
 static int trinket_hexagon_x = 0;
 static int trinket_hexagon_y = 0;
+static float camera_y_lerp_t = 0.0f;
+
+#define ANIM_DANCE     0
+#define ANIM_SPIN      1
+#define ANIM_MACARENA  2
+static int current_anim = ANIM_DANCE;
+static int animation_frame = 0;
 
 static int points = 0;
-static float time = 10.0f;
+static float time = 5.0f;
 
-#define GAME_STATE_INIT 0
-#define GAME_STATE_PLAY 1
-#define GAME_STATE_END  2
+#define GAME_STATE_INIT  0
+#define GAME_STATE_PLAY  1
+#define GAME_STATE_END   2
+#define GAME_STATE_COUNT 3
 static int game_state = GAME_STATE_INIT;
+
+static float count_timer = 0.0f;
+static int count_counter = 3;
+
+static Color start_hover_color = GREEN;
+static bool start_button_pressed = false;
+static bool start_button_hover = false;
+
+static RenderTexture2D trinket_ui_tex;
+static RenderTexture2D hexbot_ui_tex;
+
+static Color logo_c1 = ORANGE;
+static Color logo_c2 = RED;
+static Color logo_c3 = PINK;
+
+static Color lerpHue(Color c) {
+    Vector3 c_hsv = ColorToHSV(c);
+    c_hsv.x = c_hsv.x += 1.0f;
+    if (c_hsv.x >= 360.0f) c_hsv.x = 0.0f;
+    return ColorFromHSV(c_hsv.x, c_hsv.y, c_hsv.z);
+}
+
+static float lerp(float a, float b, float t) {
+    return a + t * (b - a);
+}
+
+static Vector3 vlerp(Vector3 a, Vector3 b, float t) {
+    return (Vector3) {
+        .x = lerp(a.x, b.x, t),
+        .y = lerp(a.y, b.y, t),
+        .z = lerp(a.z, b.z, t)
+    };
+}
+
+static void finishGame()
+{   
+    animation_frame = 0;
+    current_anim = ANIM_MACARENA;
+    game_state = GAME_STATE_END;
+}
 
 static void initMap()
 {
@@ -132,8 +136,54 @@ static void initMap()
             hexagon_positions[i][j].x = (float)i * distanceBetweenHexagons - hexagonPositionOffset;
             hexagon_positions[i][j].y = ((float)GetRandomValue(0, 20)) * 0.1f;
             hexagon_positions[i][j].z = (float)j * distanceBetweenHexagons - hexagonPositionOffset;
+            hexagon_status[i][j] = HEX_STATUS_GOOD;
         }
     }
+}
+
+static void initGame()
+{
+    player_position = (Vector3){ 0.0f, 0.0f, 0.0f };
+    speed = 2.0f;
+    player_velocity = (Vector3){ 0.0f, 0.0f, 0.0f };
+    player_target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    player_hexagon_x = NUM_HEXAGONS-1;
+    player_hexagon_y = NUM_HEXAGONS-1;
+    player_y_lerp_t = 1.0f;
+    player_xz_lerp_t = 0.0f;
+    trinket_hexagon_x = 0;
+    trinket_hexagon_y = 0;
+    camera_y_lerp_t = 0.0f;
+    count_timer = 0.0f;
+    count_counter = 3;
+    points = 0;
+    time = 10.0f;
+    current_anim = ANIM_DANCE;
+    animation_frame = 0;
+    end_camera_position = (Vector3){ 4.0f, 4.0f, 4.0f };
+
+    initMap();
+
+    trinket_hexagon_x = GetRandomValue(3, player_hexagon_x - 1);
+    trinket_hexagon_y = GetRandomValue(3, player_hexagon_y - 1);
+    player_position = hexagon_positions[player_hexagon_x][player_hexagon_x];
+    player_target = player_position;
+    hexagon_status[player_hexagon_x][player_hexagon_x] = HEX_STATUS_WARN;
+}
+
+static bool detectTrapped() {
+    int i_l_bound = max(0, player_hexagon_x-1);
+    int j_l_bound = max(0, player_hexagon_y-1);
+    int i_h_bound = min(NUM_HEXAGONS, i_l_bound+3);
+    int j_h_bound = min(NUM_HEXAGONS, j_l_bound+3);
+
+    for (int i=i_l_bound; i<i_h_bound; i++) {
+        for (int j=j_l_bound; j<j_h_bound; j++) {
+            if (hexagon_status[i][j] == HEX_STATUS_GOOD) return false;
+        }
+    }
+
+    return true;
 }
 
 static void moveTrinketToNext() {
@@ -161,19 +211,77 @@ static void moveTrinketToNext() {
                 if (hexagon_status[i][j] == HEX_STATUS_GOOD) {
                     trinket_hexagon_x = i;
                     trinket_hexagon_y = j;
+                    found = true;
                     break;
                 }
             }
         }
     }
+
+    if (!found) {
+        finishGame();
+    }
 }
 
 static void initFrame(void)
 {
+    Rectangle rec = {
+        .x = screenWidth / 2 - 200 - 12,
+        .y = screenHeight - 150 - 12,
+        .width = 400,
+        .height = 100
+    };
+
+    start_button_hover = CheckCollisionPointRec(GetMousePosition(), rec);
+
+    if (start_button_hover && IsMouseButtonPressed(0))
+    {
+        game_state = GAME_STATE_COUNT;
+    }
+}
+
+static void countFrame(void)
+{
+    count_timer += GetFrameTime();
+    if (count_timer >= 1.0f)
+    {
+        count_timer = 0.0f;
+        count_counter--;
+    }
+
+    if (count_counter == -1)
+    {
+        game_state = GAME_STATE_PLAY;
+    }
 }
 
 static void endFrame(void)
 {
+    Vector3 up = {0,1,0};
+    end_camera_position = Vector3RotateByAxisAngle(end_camera_position, up, 0.01f);
+    
+    if (end_camera_position.y > player_position.y + 3.0f)
+    {
+        end_camera_position.y -= GetFrameTime() * 2.0f;
+    }
+
+    camera.position = Vector3Add(end_camera_position, player_position);
+
+
+    Rectangle rec = {
+        .x = screenWidth / 2 - 200 - 12,
+        .y = screenHeight - 150 - 12,
+        .width = 400,
+        .height = 100
+    };
+
+    start_button_hover = CheckCollisionPointRec(GetMousePosition(), rec);
+
+    if (start_button_hover && IsMouseButtonPressed(0))
+    {
+        initGame();
+        game_state = GAME_STATE_INIT;
+    }
 }
 
 static void gameFrame(void)
@@ -196,7 +304,6 @@ static void gameFrame(void)
                 if (collision.hit && hexagon_status[i][j] == HEX_STATUS_GOOD) {
                     hitHexagon_i = i;
                     hitHexagon_j = j;
-                    printf("hit %d,%d\n",i,j);
                 }
             }
         }
@@ -208,34 +315,49 @@ static void gameFrame(void)
         player_hexagon_x = hitHexagon_i;
         player_hexagon_y = hitHexagon_j;
         player_target = hexagon_positions[hitHexagon_i][hitHexagon_j];
-        player_time = 0.0f;
+        player_y_lerp_t = 0.0f;
+        player_xz_lerp_t = 0.0f;
+
+        current_anim = ANIM_SPIN;
+        animation_frame = 0;
     }
 
-    player_position = Vector3SmoothDamp(
-        player_position,
-        player_target,
-        &player_velocity,
-        0.2f,
-        100.0f,
-        GetFrameTime()
-    );
-    player_position.y += 0.2f * player_time * (1.0f - player_time);
+    player_position = vlerp(player_position, player_target, player_xz_lerp_t);
+    player_xz_lerp_t = fmin(1.0f, player_xz_lerp_t + GetFrameTime() * 2.0f);
+    player_position.y += player_y_lerp_t * (1.0f - player_y_lerp_t);
 
-    if (player_time > 0.9f && player_hexagon_x == trinket_hexagon_x && player_hexagon_y == trinket_hexagon_y) {
+    if (player_y_lerp_t > 0.9f && current_anim == ANIM_SPIN) {
+        current_anim = ANIM_DANCE;
+        animation_frame = 0;
+
+        if (detectTrapped()) {
+            finishGame();
+        }
+    }
+
+    if (player_y_lerp_t > 0.9f && player_hexagon_x == trinket_hexagon_x && player_hexagon_y == trinket_hexagon_y) {
         points++;
-        time += 3.0f;
+        time += 5.0f;
+        
+        current_anim = ANIM_DANCE;
+        animation_frame = 0;
+        
         moveTrinketToNext();
     }
 
-    player_time = fmin(1.0f, player_time + GetFrameTime() * 2.0f);
+    player_y_lerp_t = fmin(1.0f, player_y_lerp_t + GetFrameTime() * 3.0f);
 
     time -= GetFrameTime();
+    if (time <= 0.0f) {
+        finishGame();
+    }
 }
 
 static void(*frame[])(void) = {
     [GAME_STATE_INIT] = initFrame,
     [GAME_STATE_PLAY] = gameFrame,
-    [GAME_STATE_END] = endFrame
+    [GAME_STATE_END] = endFrame,
+    [GAME_STATE_COUNT] = countFrame
 };
 
 static void render(void)
@@ -245,11 +367,14 @@ static void render(void)
     BeginTextureMode(target);
     ClearBackground(BLACK);
 
-    camera.target = player_position;
-    camera.position = camera.target;
-    camera.position.x += cameraTarget.x;
-    camera.position.y += cameraTarget.y;
-    camera.position.z += cameraTarget.z;
+    if (game_state != GAME_STATE_END)
+    {
+        camera.target = player_position;
+        camera.position = camera.target;
+        camera.position.x += cameraTarget.x;
+        camera.position.y += cameraTarget.y;
+        camera.position.z += cameraTarget.z;
+    }
 
     BeginMode3D(camera);
 
@@ -276,10 +401,73 @@ static void render(void)
     start_position.y += 0.5f;
     Vector3 end_position = player_position;
     end_position.y += 2.0f;
-    DrawCapsuleWires(start_position, end_position, 0.5f, 16, 3, RED);
+    
+    translation = MatrixTranslate(player_position.x, player_position.y, player_position.z);
+    x_rotation = MatrixRotateX(PI/2);
+    scale = MatrixScale(0.01f, 0.01f, 0.01f);
+    hexbot_model.transform = MatrixMultiply(scale, MatrixMultiply(x_rotation, translation));
+
+    ModelAnimation *animation;
+    int keyframe_count = 0;
+    switch (current_anim) {
+        case ANIM_DANCE:
+            keyframe_count = hexbot_dance[0].keyframeCount;
+            animation = &hexbot_dance[0];
+            break;
+        case ANIM_SPIN:
+            keyframe_count = hexbot_spin[0].keyframeCount;
+            animation = &hexbot_spin[0];
+            break;
+        case ANIM_MACARENA:
+            keyframe_count = hexbot_macarena[0].keyframeCount;
+            animation = &hexbot_macarena[0];
+            break;
+    }
+
+    animation_frame = (animation_frame + 2) % keyframe_count;
+    UpdateModelAnimation(hexbot_model, *animation, animation_frame);
+    DrawModel(hexbot_model, (Vector3){0,0,0}, 1.0f, WHITE);
+
     DrawGrid(20, 1.0f);
     EndMode3D();
+    EndTextureMode();
 
+    Camera trinket_cam = {
+        .position = (Vector3){2,2,2},
+        .target = (Vector3){0,0,0},
+        .up = (Vector3){1,0,0},
+        .fovy = 45.0f,
+        .projection = CAMERA_PERSPECTIVE
+    };
+    BeginTextureMode(trinket_ui_tex);
+    Color color_transp = WHITE;
+    color_transp.a = 0;
+    ClearBackground(color_transp);
+    BeginMode3D(trinket_cam);
+    translation = MatrixTranslate(0,0,0);
+    scale = MatrixIdentity();
+    x_rotation = MatrixRotateX(GetTime());
+    y_rotation = MatrixRotateY(GetTime());
+    trinket_model.transform = MatrixMultiply(scale, MatrixMultiply(x_rotation, MatrixMultiply(y_rotation, translation)));
+    DrawModel(trinket_model, (Vector3){0,0,0}, 1.0f, WHITE);
+    EndMode3D();
+    EndTextureMode();
+
+    BeginTextureMode(hexbot_ui_tex);
+    ClearBackground(color_transp);
+    trinket_cam.position.x = 0.0f;
+    trinket_cam.position.y = 0.0f;
+    trinket_cam.position.z = 5.0f;
+    trinket_cam.target.y = 2.0f;
+    BeginMode3D(trinket_cam);
+    translation = MatrixTranslate(0,0,0);
+    scale = MatrixIdentity();
+    x_rotation = MatrixRotateX(PI/2);
+    // y_rotation = MatrixRotateZ(GetTime());
+    scale = MatrixScale(0.02f, 0.02f, 0.02f);
+    hexbot_model.transform = MatrixMultiply(scale, MatrixMultiply(x_rotation, translation));
+    DrawModel(hexbot_model, (Vector3){0,0,0}, 1.0f, WHITE);
+    EndMode3D();
     EndTextureMode();
     
     BeginDrawing();
@@ -293,22 +481,107 @@ static void render(void)
         WHITE
     );
 
-    char pts_tx[32];
-    sprintf(pts_tx, "%d", points);
-    DrawText(pts_tx, 10, 10, 90, ORANGE);
-    DrawText(pts_tx, 12, 12, 90, RED);
-    DrawText(pts_tx, 14, 14, 90, PINK);
-    DrawText(pts_tx, 18, 18, 90, WHITE);
+    if (game_state == GAME_STATE_PLAY)
+    {
+        char pts_tx[32];
+        sprintf(pts_tx, "%d", points);
+        DrawText(pts_tx, 10, 10, 90, ORANGE);
+        DrawText(pts_tx, 12, 12, 90, RED);
+        DrawText(pts_tx, 14, 14, 90, PINK);
+        DrawText(pts_tx, 18, 18, 90, WHITE);
 
-    char time_tx[32];
-    sprintf(time_tx, "%.03f", time);
-    int time_tx_sz = MeasureText("99.999", 90);
-    int time_tx_x = screenWidth / 2 - time_tx_sz / 2;
-    int time_tx_y = screenHeight - 120;
-    DrawText(time_tx, time_tx_x, time_tx_y, 90, ORANGE);
-    DrawText(time_tx, time_tx_x + 2, time_tx_y + 2, 90, RED);
-    DrawText(time_tx, time_tx_x + 4, time_tx_y + 4, 90, PINK);
-    DrawText(time_tx, time_tx_x + 8, time_tx_y + 8, 90, WHITE);
+        char time_tx[32];
+        sprintf(time_tx, "%.03f", time);
+        int time_tx_sz = MeasureText("99.999", 90);
+        int time_tx_x = screenWidth / 2 - time_tx_sz / 2;
+        int time_tx_y = screenHeight - 120;
+        DrawText(time_tx, time_tx_x, time_tx_y, 90, ORANGE);
+        DrawText(time_tx, time_tx_x + 2, time_tx_y + 2, 90, RED);
+        DrawText(time_tx, time_tx_x + 4, time_tx_y + 4, 90, PINK);
+        DrawText(time_tx, time_tx_x + 8, time_tx_y + 8, 90, WHITE);
+
+        Color trinket_ui_pts_color = WHITE;
+        trinket_ui_pts_color.a = 160;
+        DrawTexture(trinket_ui_tex.texture, 20 + MeasureText(pts_tx, 90), 10, trinket_ui_pts_color);
+    }
+
+    if (game_state == GAME_STATE_INIT)
+    {
+        int logo_x = screenWidth - MeasureText("HEXBOT", 144) - 60;
+        int logo_y = 60;
+
+        logo_c1 = lerpHue(logo_c1);
+        logo_c2 = lerpHue(logo_c2);
+        logo_c3 = lerpHue(logo_c3);
+
+        DrawText("HEXBOT", logo_x, logo_y, 144, logo_c1);
+        DrawText("HEXBOT", logo_x + 2, logo_y + 2, 144, logo_c2);
+        DrawText("HEXBOT", logo_x + 4, logo_y + 4, 144, logo_c3);
+        DrawText("HEXBOT", logo_x + 8, logo_y + 8, 144, WHITE);
+    }
+    
+    if (game_state == GAME_STATE_INIT || game_state == GAME_STATE_END)
+    {
+        Rectangle rec = {
+            .x = screenWidth / 2 - 200 - 12,
+            .y = screenHeight - 150 - 12,
+            .width = 400,
+            .height = 100
+        };
+
+        if (start_button_hover)
+        {
+            start_hover_color = lerpHue(start_hover_color);
+            start_hover_color.a = 120;
+            DrawRectangleRounded(rec, 0.5f, 16, start_hover_color);
+        }
+
+        DrawRectangleRoundedLinesEx(rec, 0.5f, 16, 3.0f, ORANGE);
+        rec.x += 3; rec.y += 3;
+        DrawRectangleRoundedLinesEx(rec, 0.5f, 16, 3.0f, RED);
+        rec.x += 3; rec.y += 3;
+        DrawRectangleRoundedLinesEx(rec, 0.5f, 16, 3.0f, PINK);
+        rec.x += 3; rec.y += 3;
+        DrawRectangleRoundedLinesEx(rec, 0.5f, 16, 3.0f, WHITE);
+
+        int play_tx_x = rec.x + 60;
+        int play_tx_y = rec.y;
+        DrawText("PLAY", play_tx_x, play_tx_y, 100, ORANGE);
+        DrawText("PLAY", play_tx_x + 2, play_tx_y + 2, 100, RED);
+        DrawText("PLAY", play_tx_x + 4, play_tx_y + 4, 100, PINK);
+        DrawText("PLAY", play_tx_x + 8, play_tx_y + 8, 100, WHITE);
+    }
+
+    if (game_state == GAME_STATE_END)
+    {
+        char score_tx[128];
+        sprintf(score_tx, "GOT %d", points);
+        int score_tx_sz = MeasureText(score_tx, 100);
+        int score_tx_x = screenWidth / 2 - score_tx_sz / 2 - 50;
+        int score_tx_y = screenHeight / 2;
+        DrawText(score_tx, score_tx_x, score_tx_y, 100, ORANGE);
+        DrawText(score_tx, score_tx_x + 2, score_tx_y + 2, 100, RED);
+        DrawText(score_tx, score_tx_x + 4, score_tx_y + 4, 100, PINK);
+        DrawText(score_tx, score_tx_x + 8, score_tx_y + 8, 100, WHITE);
+        DrawTexture(trinket_ui_tex.texture, score_tx_x + score_tx_sz + 20, score_tx_y, WHITE);
+    }
+
+    if (game_state == GAME_STATE_COUNT)
+    {
+        char counter_tx[32];
+        
+        count_counter > 0 ?
+            sprintf(counter_tx, "%d", count_counter) :
+            sprintf(counter_tx, "GO!");
+
+        int counter_tx_sz = MeasureText(counter_tx, 160);
+        int counter_tx_x = screenWidth / 2 - counter_tx_sz / 2;
+        int counter_tx_y = screenHeight / 2 + 40;
+        DrawText(counter_tx, counter_tx_x, counter_tx_y, 160, ORANGE);
+        DrawText(counter_tx, counter_tx_x + 2, counter_tx_y + 2, 160, RED);
+        DrawText(counter_tx, counter_tx_x + 4, counter_tx_y + 4, 160, PINK);
+        DrawText(counter_tx, counter_tx_x + 8, counter_tx_y + 8, 160, WHITE);
+    }
 
     EndDrawing();
 }
@@ -320,24 +593,28 @@ int main(void)
     SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messages
 #endif
     InitWindow(screenWidth, screenHeight, "raylib gamejam template");
+    SetTargetFPS(60);
     target = LoadRenderTexture(screenWidth / 3, screenHeight / 3);
     SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
 
+    trinket_ui_tex = LoadRenderTexture(100, 100);
+    hexbot_ui_tex = LoadRenderTexture(128, 128);
+    SetTextureFilter(trinket_ui_tex.texture, TEXTURE_FILTER_POINT);
+
     SetRandomSeed(GetFPS() * 10);
 
+    int num_animations = 0;
     hexagon_model = LoadModel("resources/hexagon.glb");
     trinket_model = LoadModel("resources/trinket.glb");
+    hexbot_model = LoadModel("resources/dance.glb");
+    hexbot_dance = LoadModelAnimations("resources/dance.glb", &num_animations);
+    hexbot_spin = LoadModelAnimations("resources/spinning.glb", &num_animations);
+    hexbot_macarena = LoadModelAnimations("resources/macarena.glb", &num_animations);
 
-    initMap();
-
-    trinket_hexagon_x = GetRandomValue(3, player_hexagon_x - 1);
-    trinket_hexagon_y = GetRandomValue(3, player_hexagon_y - 1);
-    player_position = hexagon_positions[player_hexagon_x][player_hexagon_x];
-    player_target = player_position;
-    hexagon_status[player_hexagon_x][player_hexagon_x] = HEX_STATUS_WARN;
+    initGame();
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(render, 0, 1);
+    emscripten_set_main_loop(render, 60, 1);
 #else
     SetTargetFPS(60);     // Set our game frames-per-second
 
