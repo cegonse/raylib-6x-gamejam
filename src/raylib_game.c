b7b469ecf3c04a48938f893bcacb25e62c4bdb39
César González Segura <contact@cesargonzalez.dev>
2026-07-10 16:25:58 +0200

    Day three

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -23,6 +23,7 @@
 #include <stdio.h>                          // Required for: printf()
 #include <stdlib.h>                         // Required for: 
 #include <string.h>                         // Required for:
+#include <memory.h>
 
 #define SUPPORT_LOG_INFO
 #if defined(SUPPORT_LOG_INFO)
@@ -40,9 +41,10 @@ static ModelAnimation *hexbot_dance;
 static ModelAnimation *hexbot_spin;
 static ModelAnimation *hexbot_macarena;
 
-#define HEX_STATUS_GOOD 0
-#define HEX_STATUS_WARN 1
-#define HEX_STATUS_BAD  2
+#define HEX_STATUS_GOOD    0
+#define HEX_STATUS_WARN    1
+#define HEX_STATUS_BAD     2
+#define HEX_STATUS_MERGED  3
 
 #define NUM_HEXAGONS 8
 static Vector3 hexagon_positions[NUM_HEXAGONS][NUM_HEXAGONS];
@@ -78,7 +80,7 @@ static int current_anim = ANIM_DANCE;
 static int animation_frame = 0;
 
 static int points = 0;
-static float time = 5.0f;
+static float time = 1.0f;
 
 #define GAME_STATE_INIT  0
 #define GAME_STATE_PLAY  1
@@ -102,9 +104,39 @@ static Color logo_c3 = PINK;
 
 static Rectangle play_button_rec;
 
-static Color lerpHue(Color c) {
+static Vector2 not_found = { -1, -1 };
+
+typedef struct {
+    Vector2 position;
+    Vector2 original_coords[16];
+    Vector2 original_positions[16];
+    int num_originals;
+    float scale;
+    float scale_lerp_t;
+    Color color;
+    float sync_offset;
+    float angle;
+} MergedHexagon;
+#define MAX_MERGED_HEXAGONS   128
+static MergedHexagon merged_hexagons[MAX_MERGED_HEXAGONS];
+static int num_merged_hexagons;
+static Color random_color_table[] = {
+    RED,
+    PINK,
+    BROWN,
+    BLUE,
+    YELLOW,
+    SKYBLUE,
+    ORANGE,
+    MAGENTA,
+};
+
+static int merge_text_position = 800;
+static float merge_timer = 1.5f;
+
+static Color lerpHue(Color c, float speed) {
     Vector3 c_hsv = ColorToHSV(c);
-    c_hsv.x = c_hsv.x += 1.0f;
+    c_hsv.x = c_hsv.x += 1.0f * speed;
     if (c_hsv.x >= 360.0f) c_hsv.x = 0.0f;
     return ColorFromHSV(c_hsv.x, c_hsv.y, c_hsv.z);
 }
@@ -159,10 +191,13 @@ static void initGame()
     count_timer = 0.0f;
     count_counter = 3;
     points = 0;
-    time = 10.0f;
+    time = 100.0f;
     current_anim = ANIM_DANCE;
     animation_frame = 0;
     end_camera_position = (Vector3){ 4.0f, 4.0f, 4.0f };
+    num_merged_hexagons = 0;
+    memset(merged_hexagons, 0, sizeof(merged_hexagons));
+    merge_timer = 1.5f;
 
     initMap();
 
@@ -186,6 +221,113 @@ static bool detectTrapped() {
     }
 
     return true;
+}
+
+static Vector2 xyMean(Vector2 *v, int c)
+{
+    Vector2 ret = {0};
+
+    for (int i=0; i<c; i++) {
+        ret.x += v[i].x;
+        ret.y += v[i].y;
+    }
+
+    ret.x /= (float)c;
+    ret.y /= (float)c;
+
+    return ret;
+}
+
+static void mergeHexagons()
+{
+    for (int i=NUM_HEXAGONS-1; i>=1; i--) {
+        for (int j=NUM_HEXAGONS-1; j>=1; j--) {
+            int x, y;
+            MergedHexagon merged = {0};
+
+            if (hexagon_status[i][j] != HEX_STATUS_BAD) continue;
+
+            x = i-1; y=j;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i+1; y=j;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i; y=j-1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i; y=j+1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i+1; y=j+1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i+1; y=j-1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i-1; y=j+1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            x = i-1; y=j-1;
+            if (hexagon_status[x][y] == HEX_STATUS_BAD) {
+                merged.original_coords[merged.num_originals] = (Vector2){x,y};
+                merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[x][y].x, hexagon_positions[x][y].z};
+                merged.num_originals++;
+            }
+
+            merged.original_coords[merged.num_originals] = (Vector2){i,j};
+            merged.original_positions[merged.num_originals] = (Vector2){hexagon_positions[i][j].x, hexagon_positions[i][j].z};
+            merged.num_originals++;
+
+            printf("num orig: %d\n", merged.num_originals);
+
+            if (merged.num_originals < 4 || merged.num_originals % 2 != 0) continue;
+
+            merged.position = xyMean(merged.original_positions, merged.num_originals);
+            merged.color = random_color_table[GetRandomValue(0, 7)];
+            merged.sync_offset = (float)GetRandomValue(0, 20);
+            merged.angle = ((float)GetRandomValue(0, 359)) * DEG2RAD;
+            
+            merged_hexagons[num_merged_hexagons++] = merged;
+            for (int aa=0; aa<merged.num_originals; aa++) printf("orig[%d]: %f,%f\n", aa, merged.original_positions[aa].x, merged.original_positions[aa].y);
+            printf("merged pos: %f,%f\n", merged.position.x, merged.position.y);
+            
+            for (int i=0; i<merged.num_originals; i++) {
+                hexagon_status[(int)merged.original_coords[i].x][(int)merged.original_coords[i].y] = HEX_STATUS_MERGED;
+            }
+
+            merge_timer = 0.0f;
+            merge_text_position = 800;
+        }
+    }
 }
 
 static void moveTrinketToNext() {
@@ -274,7 +416,7 @@ static void endFrame(void)
 static void gameFrame(void)
 {
     int hitHexagon_i = -1, hitHexagon_j = -1;
-    if (IsMouseButtonPressed(0))
+    if (IsMouseButtonPressed(0) && player_y_lerp_t >= 0.8f)
     {
         Ray ray = GetScreenToWorldRay(GetMousePosition(), camera);
 
@@ -313,16 +455,18 @@ static void gameFrame(void)
     player_xz_lerp_t = fmin(1.0f, player_xz_lerp_t + GetFrameTime() * 2.0f);
     player_position.y += player_y_lerp_t * (1.0f - player_y_lerp_t);
 
-    if (player_y_lerp_t > 0.9f && current_anim == ANIM_SPIN) {
+    if (player_y_lerp_t > 0.7f && current_anim == ANIM_SPIN) {
         current_anim = ANIM_DANCE;
         animation_frame = 0;
+
+        mergeHexagons();
 
         if (detectTrapped()) {
             finishGame();
         }
     }
 
-    if (player_y_lerp_t > 0.9f && player_hexagon_x == trinket_hexagon_x && player_hexagon_y == trinket_hexagon_y) {
+    if (player_y_lerp_t > 0.8f && player_hexagon_x == trinket_hexagon_x && player_hexagon_y == trinket_hexagon_y) {
         points++;
         time += 5.0f;
         
@@ -338,6 +482,11 @@ static void gameFrame(void)
     if (time <= 0.0f) {
         finishGame();
     }
+
+    if (merge_timer <= 1.5f) {
+        merge_timer += GetFrameTime();
+        merge_text_position -= 1500 * GetFrameTime();
+    }
 }
 
 static void(*frame[])(void) = {
@@ -350,6 +499,10 @@ static void(*frame[])(void) = {
 static void render(void)
 {
     frame[game_state]();
+
+    logo_c1 = lerpHue(logo_c1, 1.0f);
+    logo_c2 = lerpHue(logo_c2, 1.0f);
+    logo_c3 = lerpHue(logo_c3, 1.0f);
 
     BeginTextureMode(target);
     ClearBackground(BLACK);
@@ -370,8 +523,26 @@ static void render(void)
             Color hex_color = GREEN;
             if (hexagon_status[i][j] == HEX_STATUS_WARN) hex_color = ORANGE;
             if (hexagon_status[i][j] == HEX_STATUS_BAD) hex_color = RED;
+            if (hexagon_status[i][j] == HEX_STATUS_MERGED) continue;
+
             DrawModel(hexagon_model, hexagon_positions[i][j], 1.0f, hex_color);
         }
+    }
+
+    for (int i=0; i<num_merged_hexagons; i++) {
+        Vector3 pos;
+
+        for (int j=0; j<merged_hexagons[i].num_originals; j++) {
+            Vector2 pp = merged_hexagons[i].original_positions[j];
+            pos = (Vector3){pp.x, 1.0f + sinf(GetTime() + merged_hexagons[i].sync_offset), pp.y};
+            merged_hexagons[i].color = lerpHue(merged_hexagons[i].color, 0.3f);
+    
+            DrawModelEx(hexagon_model, pos, (Vector3){0}, 0.0f, (Vector3){1.5f,1,1.5f}, merged_hexagons[i].color);
+        }
+
+        pos = (Vector3){merged_hexagons[i].position.x, 1.0f + sinf(GetTime() + merged_hexagons[i].sync_offset + 15), merged_hexagons[i].position.y};
+        DrawModelEx(hexagon_model, pos, (Vector3){0,1,0}, merged_hexagons[i].angle, (Vector3){1.5f,0.6,1.5f}, merged_hexagons[i].color);
+        merged_hexagons[i].angle += GetFrameTime() * 50.0f;
     }
 
     Vector3 trinket_position = hexagon_positions[trinket_hexagon_x][trinket_hexagon_y];
@@ -382,7 +553,10 @@ static void render(void)
     Matrix x_rotation = MatrixRotateX(GetTime());
     Matrix y_rotation = MatrixRotateY(GetTime());
     trinket_model.transform = MatrixMultiply(scale, MatrixMultiply(x_rotation, MatrixMultiply(y_rotation, translation)));
-    DrawModel(trinket_model, (Vector3){0,0,0}, 1.0f, WHITE);
+
+    if (game_state == GAME_STATE_PLAY || (game_state == GAME_STATE_END && trinket_hexagon_x != player_hexagon_x && trinket_hexagon_y != player_hexagon_y)) {
+        DrawModel(trinket_model, (Vector3){0,0,0}, 1.0f, WHITE);
+    }
 
     Vector3 start_position = player_position;
     start_position.y += 0.5f;
@@ -490,16 +664,19 @@ static void render(void)
         Color trinket_ui_pts_color = WHITE;
         trinket_ui_pts_color.a = 160;
         DrawTexture(trinket_ui_tex.texture, 20 + MeasureText(pts_tx, 90), 10, trinket_ui_pts_color);
+
+        if (merge_text_position < 800) {
+            DrawText("MEEEERGE!!!!!", merge_text_position,     sinf(GetTime() * 5)*50 + 150, 200,     logo_c1);
+            DrawText("MEEEERGE!!!!!", merge_text_position + 2, sinf(GetTime() * 5)*50 + 150 + 2, 200, logo_c2);
+            DrawText("MEEEERGE!!!!!", merge_text_position + 4, sinf(GetTime() * 5)*50 + 150 + 4, 200, logo_c3);
+            DrawText("MEEEERGE!!!!!", merge_text_position + 8, sinf(GetTime() * 5)*50 + 150 + 8, 200, WHITE);
+        }
     }
 
     if (game_state == GAME_STATE_INIT)
     {
         int logo_x = screenWidth - MeasureText("HEXBOT", 144) - 60;
         int logo_y = 60;
-
-        logo_c1 = lerpHue(logo_c1);
-        logo_c2 = lerpHue(logo_c2);
-        logo_c3 = lerpHue(logo_c3);
 
         DrawText("HEXBOT", logo_x, logo_y, 144, logo_c1);
         DrawText("HEXBOT", logo_x + 2, logo_y + 2, 144, logo_c2);
@@ -515,7 +692,7 @@ static void render(void)
 
         if (start_button_hover)
         {
-            start_hover_color = lerpHue(start_hover_color);
+            start_hover_color = lerpHue(start_hover_color, 1.0f);
             start_hover_color.a = 120;
             DrawRectangleRounded(rec, 0.5f, 16, start_hover_color);
         }
@@ -542,12 +719,22 @@ static void render(void)
         sprintf(score_tx, "GOT %d", points);
         int score_tx_sz = MeasureText(score_tx, 100);
         int score_tx_x = screenWidth / 2 - score_tx_sz / 2 - 50;
-        int score_tx_y = screenHeight / 2;
+        int score_tx_y = screenHeight / 2 + 20;
         DrawText(score_tx, score_tx_x, score_tx_y, 100, ORANGE);
         DrawText(score_tx, score_tx_x + 2, score_tx_y + 2, 100, RED);
         DrawText(score_tx, score_tx_x + 4, score_tx_y + 4, 100, PINK);
         DrawText(score_tx, score_tx_x + 8, score_tx_y + 8, 100, WHITE);
         DrawTexture(trinket_ui_tex.texture, score_tx_x + score_tx_sz + 20, score_tx_y, WHITE);
+
+        char merged_tx[128];
+        sprintf(merged_tx, "MERGED %d", num_merged_hexagons);
+        int merged_tx_sz = MeasureText(merged_tx, 100);
+        int merged_tx_x = screenWidth / 2 - merged_tx_sz / 2;
+        int merged_tx_y = 40;
+        DrawText(merged_tx, merged_tx_x, merged_tx_y, 100, ORANGE);
+        DrawText(merged_tx, merged_tx_x + 2, merged_tx_y + 2, 100, RED);
+        DrawText(merged_tx, merged_tx_x + 4, merged_tx_y + 4, 100, PINK);
+        DrawText(merged_tx, merged_tx_x + 8, merged_tx_y + 8, 100, WHITE);
     }
 
     if (game_state == GAME_STATE_COUNT)
